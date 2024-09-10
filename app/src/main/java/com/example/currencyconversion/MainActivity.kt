8e91a05464ced8b4052a6d14a1ce5bd0f20fa391
ST10033475 <ST10033475@vcconnect.edu.za>
2024-09-10 08:33:56 +0200

    First Commit

--- app/src/main/java/com/example/currencyconversion/MainActivity.kt
+++ app/src/main/java/com/example/currencyconversion/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.currencyconversion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var ConvertButton : Button
+
+    private lateinit var Amt : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ConvertButton = findViewById(R.id.Convert)
+        ConvertButton.setOnClickListener{
+            val inputCurr = findViewById(R.id.InputCurrency) as EditText
+            val outputCurr = findViewById(R.id.OutputCurrency) as EditText
+            val inputAmt = findViewById(R.id.InputAmount) as EditText
+            val InputAmount = inputAmt.text.toString()
+            val InputCurrency = inputCurr.text.toString()
+            val OutputCurrency = outputCurr.text.toString()
 
     }
-}
+}}
