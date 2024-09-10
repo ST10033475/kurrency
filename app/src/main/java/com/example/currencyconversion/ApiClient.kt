d60e7dcf472e3400b82e324729c2e47c00f642a2
ST10033475 <ST10033475@vcconnect.edu.za>
2024-09-10 08:55:17 +0200

    Commit

--- app/src/main/java/com/example/currencyconversion/ApiClient.kt
+++ app/src/main/java/com/example/currencyconversion/ApiClient.kt
@@ -2,7 +2,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+    private const val BASE_URL = "https://api.getgeoapi.com/v2/currency/convert"
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
